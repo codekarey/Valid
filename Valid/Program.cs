889f63f91a535ff1f2ae0926af45abfdc2e087f7
codekarey <codekarey@gmail.com>
2020-02-12 19:34:05 -0500

    Basic idea outline

---
+++ Valid/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Valid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //loop on request
+            bool yes = true;
+            while (yes)
+            {
+            Console.WriteLine("Please enter your information below to be validated.");
+            //fullname
+            Name();
+            //email
+            Email();
+            //phone number
+            Phone();
+            //date of birth
+            Dob();
+            //current time
+            Time();
+            }
+        }
+        public static bool Loop()
+        {
+
+        }
+        public static string Name()
+        {
+
+        }
+        public static string Email()
+        {
+
+        }
+        public static string Phone()
+        {
+
+        }
+        public static string Dob()
+        {
+
+        }
+        public static string Time()
+        {
+
+        }
+    }
+}
